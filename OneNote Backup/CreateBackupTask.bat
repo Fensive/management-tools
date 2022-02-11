2a2e2f2de70d49e291f497dcd0f7ce6ca7fa23ec
J3z4 <elidmashiah@gmail.com>
2022-02-12 00:34:05 +0200

    removed unnecessary quotes on the second line

--- OneNote Backup/CreateBackupTask.bat
+++ OneNote Backup/CreateBackupTask.bat
@@ -1,2 +1,2 @@
 set script_location="D:\OneDrive - Fensive Security\management-tools\OneNote Backup\OneNoteBackup.ps1"
-SCHTASKS /CREATE /SC DAILY /TN "FensiveTasks\OneNoteBackup" /TR "powershell -executionpolicy bypass -windowstyle Hidden -File """%script_location%"""" /ST 20:00 /RU %username%
+SCHTASKS /CREATE /SC DAILY /TN "FensiveTasks\OneNoteBackup" /TR "powershell -executionpolicy bypass -windowstyle Hidden -File ""%script_location%""" /ST 20:00 /RU %username%
